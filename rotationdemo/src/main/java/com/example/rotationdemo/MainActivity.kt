6316e865d9f13466613578b9d9b04bb99fd0f206
smirnov_aa <smirnov_aa@vector.ru>
2026-01-31 18:36:13 +0300

    Добавил модуль RotationDemo

---
+++ rotationdemo/src/main/java/com/example/rotationdemo/MainActivity.kt
@@ -0,0 +1,87 @@
+/*
+В книге Нил Смита это соответствует проекту AnimateState (глава 44)
+ */
+package com.example.rotationdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.rotationdemo.ui.theme.RotationDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RotationDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    RotationDemo()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RotationDemo() {
+    var rotated by remember { mutableStateOf(false) }
+
+    val angle by animateFloatAsState(
+        targetValue = if (rotated) 360f else 0f,
+        //easing = LinearEasing и пр. опции можно добавить при желании
+        animationSpec = tween(durationMillis = 3000/*, easing = LinearEasing*/), label = "Rotate"
+    )
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth())
+    {
+        Image(
+            painter = painterResource(R.drawable.propeller),
+            contentDescription = "fan",
+            modifier = Modifier
+                .rotate(angle)
+                .padding(10.dp)
+                .size(300.dp)
+        )
+
+        Button(
+            onClick = { rotated = !rotated },
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(text = "Rotate Propeller")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RotationDemoTheme {
+        RotationDemo()
+    }
+}
