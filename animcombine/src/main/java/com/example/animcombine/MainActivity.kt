12c9bc6214ebf8a9ca7d0196193a7464fe098f1f
smirnov_aa <smirnov_aa@vector.ru>
2026-02-01 19:32:30 +0300

    Добавил модуль CanvasDemo (линии, прямоугольники, поворот фигуры)

--- animcombine/src/main/java/com/example/animcombine/MainActivity.kt
+++ animcombine/src/main/java/com/example/animcombine/MainActivity.kt
@@ -43,6 +43,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.animcombine.ui.theme.AnimCombineTheme
@@ -126,5 +127,14 @@ fun AnimCombine() {
         ) {
             Text(text = "Start Animation")
         }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    AnimCombineTheme {
+        AnimCombine()
     }
 }
